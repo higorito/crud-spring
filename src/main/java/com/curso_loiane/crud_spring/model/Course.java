83d6a0cc2ddc3d12556f51c76dfafc7e86605283
higor <higorps198@gmail.com>
2024-07-01 16:09:12 -0300

    feat: bd h2 e conectando no angular

--- src/main/java/com/curso_loiane/crud_spring/model/Course.java
+++ src/main/java/com/curso_loiane/crud_spring/model/Course.java
@@ -1,5 +1,6 @@
 package com.curso_loiane.crud_spring.model;
 
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.*;
 import lombok.*;
 
@@ -14,6 +15,7 @@ import lombok.*;
 public class Course {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
+    @JsonProperty("_id")
     private long id;
 
     @Column(length = 200, nullable = false) //quanto mais especifico melhor p/ validação
