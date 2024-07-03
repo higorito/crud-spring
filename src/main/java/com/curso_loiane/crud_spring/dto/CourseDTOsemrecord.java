38d47820bf46d3330838459ea33683bbf0e8892e
higor <higorps198@gmail.com>
2024-07-03 12:05:32 -0300

    feat: add padrao DTO

---
+++ src/main/java/com/curso_loiane/crud_spring/dto/CourseDTOsemrecord.java
@@ -0,0 +1,32 @@
+package com.curso_loiane.crud_spring.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+
+@Data
+public class CourseDTOsemrecord {
+
+    //ai nesse caso aqui antes do java 14 que nao tem record
+    //ai eu teria que refatorar o codigo para aceitar a classe CourseDTOsemrecord
+
+    private long _id;
+
+    @NotNull
+    @NotBlank
+    @Length(min = 3, max = 60)
+    private String name;
+
+    @NotNull
+    @Size(max = 30)
+    @Pattern(regexp = "Backend|Frontend|Mobile")
+    private String category;
+
+    @NotNull
+    @Length(max = 10)
+    @Pattern(regexp = "Ativo|Inativo")
+    private String status = "Ativo";
+}
---
+++ src/main/java/com/curso_loiane/crud_spring/dto/CouseDTO.java
@@ -0,0 +1,19 @@
+package com.curso_loiane.crud_spring.dto;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
+import org.hibernate.validator.constraints.Length;
+
+//nao consegue usar o record com o jpa, pq nao é considerado uma entidade
+//contrutor completo
+//getters e setters
+//----------------------------------
+//as vezes pode usar 2 dto, um para entrada e outro para saida
+public record CouseDTO(
+        @JsonProperty("_id") Long id,
+        @NotNull @NotBlank @Length(min = 3, max = 60) String name,
+        @NotNull @Length(max = 10) @Pattern(regexp = "Backend|Frontend|Mobile") String category
+        //@NotNull @Length(max = 10) @Pattern(regexp = "Ativo|Inativo") String status) pq aqui nao precisa *expor*
+        )   { }
