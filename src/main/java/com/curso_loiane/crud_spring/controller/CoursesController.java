467ba0b0a9ec179f008449fc43e0aff0a7448aa3
higor <higorps198@gmail.com>
2024-07-03 11:12:31 -0300

    feat: adicionando camada de service

--- src/main/java/com/curso_loiane/crud_spring/controller/CoursesController.java
+++ src/main/java/com/curso_loiane/crud_spring/controller/CoursesController.java
@@ -2,6 +2,7 @@ package com.curso_loiane.crud_spring.controller;
 
 import com.curso_loiane.crud_spring.model.Course;
 import com.curso_loiane.crud_spring.repository.CourseRepository;
+import com.curso_loiane.crud_spring.service.CourseService;
 import jakarta.validation.Valid;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -17,58 +18,49 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/api/courses")
-@AllArgsConstructor
+//@AllArgsConstructor
 @Validated //pro controller aceitar validações, do not null, positive. se nao tiver o @Valid ja da conta
 public class CoursesController {
 
-    //@Autowired pode ser assim, mas quando for obrigatorio, é melhor colocar no construtor, ai faz manual ou via lombok
-    private CourseRepository courseRepository;
+    private final CourseService courseService;
 
-    /*public CoursesController(CourseRepository courseRepository){
-        this.courseRepository = courseRepository;
-    }/*/
-
+    public CoursesController(CourseService courseService) {
+        this.courseService = courseService;
+    }
 
     @GetMapping
     public List<Course> list(){
-        return courseRepository.findAll();
+        return courseService.list();
     }
 
     //@RequestMapping(method = RequestMethod.POST)
     @PostMapping
     @ResponseStatus(code = HttpStatus.CREATED)
-    public Course create(@RequestBody @Valid Course course){ //se for do outro jeito é ResponseEntity<Course> e sem o @ResponseStatus
-        //ja retorna o status 201
-        //return ResponseEntity.status(HttpStatus.CREATED).body(courseRepository.save(course));
-        return courseRepository.save(course);
+    public Course create(@RequestBody @Valid Course course){
+        return courseService.create(course);
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Course> findById(@PathVariable @NotNull @Positive Long id){
-        return courseRepository.findById(id)
+        return courseService.findById(id)
                 .map( recordFound-> ResponseEntity.ok().body(recordFound))
                 .orElse(ResponseEntity.notFound().build());
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<Course> update(@PathVariable @NotNull @Positive Long id, @RequestBody @Valid Course course){
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    recordFound.setName(course.getName());
-                    recordFound.setCategory(course.getCategory());
-                    Course updated = courseRepository.save(recordFound); //ja sabe que vai receber update
-                    return ResponseEntity.ok().body(updated);
-                }).orElse(ResponseEntity.notFound().build());
+        return courseService.update(id, course)
+                .map(recordFound ->
+                        ResponseEntity.ok().body(recordFound))
+                .orElse(ResponseEntity.notFound().build());
 
     }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> delete(@PathVariable @NotNull @Positive Long id){
-        return courseRepository.findById(id)
-                .map(recordFound -> {
-                    courseRepository.deleteById(id);
-                    return ResponseEntity.noContent().<Void>build();
-                }).orElse(ResponseEntity.notFound().build());
+        return courseService.delete(id) ?
+                ResponseEntity.noContent().<Void>build() :
+                ResponseEntity.notFound().<Void>build();
     }
 
 }
